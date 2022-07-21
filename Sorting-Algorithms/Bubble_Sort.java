2dfbab3ae87f65dd847eeadf523a50a6ad1197aa
unknown <you@example.com>
2022-07-21 20:38:49 +0530

    Bubble Sort Updated

--- Sorting-Algorithms/Bubble_Sort.java
+++ Sorting-Algorithms/Bubble_Sort.java
@@ -1,14 +1,34 @@
+import java.sql.Array;
+import java.util.Scanner;
+
 public class Bubble_Sort {
     public static void main(String[] args) {
+
+
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter No of Elements In an Array : ");
-        int n=sc.nextInt();
-        int[] arr =new int(n);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i]=sc.nextInt();
         }
-        System.out.print("Enter The Target Element : ");
-        int target = sc.nextInt();
-        System.out.println(bubble_sort(arr,target));
+
+        bubble_sort(arr);
+    }
+
+    private static void  bubble_sort(int[] arr) {
+            int size = arr.length;
+        for (int i = 0; i < size-1; i++) {
+            for (int j = 0; j < size-1; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
