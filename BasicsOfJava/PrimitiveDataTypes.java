304b911f6033af23d9b836a5de7a02f9f66af1bf
unknown <you@example.com>
2022-08-08 15:10:33 +0530

    New File Is Added

---
+++ BasicsOfJava/PrimitiveDataTypes.java
@@ -0,0 +1,21 @@
+//import java.lang.String;
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        int num1=10;
+        char alphabet='a';
+        byte num=10;
+        boolean n=true;
+
+        float numb = 5.5f;
+        double numc = 10.5;
+        String s="Nithish";
+
+        System.out.println(num1);
+        System.out.println(alphabet);
+        System.out.println(num);
+        System.out.println(n);
+        System.out.println(numb);
+        System.out.println(numc);
+        System.out.println(s);
+    }
+}
